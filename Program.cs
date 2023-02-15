f332f5dea31df5dfa9f8dce44c608126ab82797a
CSmith3723 <123496273+CSmith3723@users.noreply.github.com>
2023-02-15 18:37:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+class FishLengths
+
+{
+    static void Main()
+    {
+        int fishLength1 = 2;
+        int fishLength2 = 44;
+        int fishLength3 = 27;
+        int fishLength4 = 18;
+        int fishLength5 = 25;
+
+        double avgFishLength = (fishLength1 + fishLength2 + fishLength3 + fishLength4 + fishLength5) / (double)5;
+        string fishLengthString = avgFishLength.ToString("F1");
+
+        WriteLine("The average length of the five fish measured is {0} cm.", fishLengthString);
+       
+    }
+}
